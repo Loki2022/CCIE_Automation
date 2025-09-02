81f9cf19184dc7949ec46df6dbd5d7e6b811657c
Your Name <you@example.com>
2025-09-02 21:27:49 +0530

    tst1

---
+++ tst1.py
@@ -0,0 +1,17 @@
+from netmiko import ConnectHandler
+
+with open('config') as f:
+    send_cmd = (f.read().splitlines())
+
+with open('device_list') as f:
+    devices = (f.read().splitlines())
+
+for device in devices:
+    print('connecting to ' + device) 
+    ip_add = device
+    
+ios_device = {'device_type':'cisco_ios','ip': ip_add , 'username': 'admin','password': 'cisco'}
+
+net_connect = ConnectHandler (**ios_device)
+output = net_connect.send_config_set(send_cmd)
+print(output)
