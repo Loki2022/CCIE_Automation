2457080bf0b2593e395894f9b12c34911d190b18
Loki2022 <kamleshr@nexaria.com>
2025-09-20 22:03:04 +0530

    new practice

--- sw12.py
+++ sw12.py
@@ -3,5 +3,18 @@ from netmiko import ConnectHandler
 sw3 = {'ip':'192.168.1.43', 'username':'admin','password':'cisco', 'device_type' :'cisco_ios'}
 sw4 = {'ip':'192.168.1.44', 'username':'admin','password':'cisco', 'device_type' :'cisco_ios'}
 
+lst = [sw3,sw4]
+
 with open('psec')as f:
-    
+    send_cmd = (f.read().splitlines())
+
+for i in lst:
+    print('connecting to '+ str(i['ip']))
+    connect = ConnectHandler(**i)
+    output = connect.send_config_set(send_cmd)
+    print(output)
+    for vlan in range (2,15):
+        cmd = ['vlan ' + str(vlan)]
+        out = connect.send_config_set(cmd)
+        print(out)
+        
