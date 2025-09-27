69465970de2c04b1f60ae18a379aa40a6470be62
Loki2022 <kamleshr@nexaria.com>
2025-09-28 00:03:24 +0530

    28

--- s65.py
+++ s65.py
@@ -4,7 +4,7 @@ from getpass import getpass
 username = input('enter username: ')
 password = getpass()
 
-with open ('s65') as f:
+with open ('s34') as f:
     sw = (f.read().splitlines())
 
 for i in sw:
@@ -12,7 +12,10 @@ for i in sw:
     ip_add = i
     ios = {'ip':ip_add, 'username': username, 'password': password, 'device_type':'cisco_ios'}
     connect = ConnectHandler(**ios)
-    for j in range(11,21):
-        send = ['vlan ' + str(j)]
-        output = connect.send_config_set(send)
-        print(output)
+    output = connect.send_command('sh vlan brief')
+    print(output)
+    
+    # for j in range(2.11):
+    #     send = ['vlan ' + str(j)]
+    #     output = connect.send_config_set(send)
+    #     print(output)
