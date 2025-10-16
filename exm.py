72fa76adc0e185324cc10bd974b36b1ed673fff6
Loki2022 <kamleshr@nexaria.com>
2025-10-16 21:11:47 +0530

    long time break

---
+++ exm.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+sw ={'ip':input('enter ip: '), 'username': username, 'password': password, 'device_type': 'cisco_ios'}
+
+connect = ConnectHandler(**sw)
+for i in range(1,11):
+    print('connecting to '+ str('ip'))
+    cmd = ['vlan '+ str(i)]
+    out = connect.send_config_set(cmd)
+    print(out)
+    send_cmd = ['int g0/0', 'switchport trunk encap dot1', 'switchport mode trunk','switchport nonegotiate']
+    out = connect.send_config_set(send_cmd)
+    print(out)
+
+
+output = connect.send_command('sh vlan brief')
+print(output)
